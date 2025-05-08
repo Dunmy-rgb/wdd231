7cb817586105e52cd991ab227a5f00f19bfac7c7
Dunmy <dunmyyoung@gmail.com>
2025-05-08 11:02:51 +0100

    final

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const nav = document.querySelector("nav");
+
+  hamburger.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
+});
+
+
 document.getElementById("lastModified").textContent = document.lastModified;
 
 function filterCourses(category) {
