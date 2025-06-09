3b1d6cd69f949c2f160d8d2330e6d652425089b5
Dunmy <dunmyyoung@gmail.com>
2025-06-09 14:25:29 +0100

    done

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         card.innerHTML = `
           <figure>
-            <img src="${item.image}" alt="${item.title}" width="300" height="200" loading="lazy">
+            <img src="${item.image}" alt="${item.title}" width="300" height="200" loading="eager">
           </figure>
           <div class="card-content">
             <h2>${item.title}</h2>
